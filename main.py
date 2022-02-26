1b0f6d2a32f153583518c0604a1ec4102ea32604
kaneru <danielck@vt.edu>
2022-02-25 21:35:31 -0500

    subreddit scrape successful

---
+++ main.py
@@ -0,0 +1,27 @@
+#! usr/bin/env python3
+import praw
+import pandas as pd
+import datetime as dt
+
+# secret keys that we need to hide
+personal_use = '5FghdU95WOsds1gwFy2Gbw'
+secret = 'LlWpk1fsV_0-UFMQXq58xH8atBRxMQ'
+
+# API for praw is here
+# https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html#praw.models.Subreddit
+# create a reddit instance
+reddit = praw.Reddit(client_id=personal_use,
+\
+                     client_secret=secret, \
+                     user_agent='FreeHub', \
+                     username='freeehub', \
+                     password='vthacks9')
+
+# get a subreddit instance
+subreddit = reddit.subreddit('freegames')
+top_subreddit = subreddit.top("week")
+
+# loop through submissions based on the parameters
+# and print the title/id
+for submission in top_subreddit:
+    print(submission.title, submission.id)
